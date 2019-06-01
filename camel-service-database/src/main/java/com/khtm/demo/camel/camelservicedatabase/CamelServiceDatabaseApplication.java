e6bf5c72e46a9d966eb57722d6ee6f6639763a0e
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-06-01 14:26:50 +0430

    Init

---
+++ camel-service-database/src/main/java/com/khtm/demo/camel/camelservicedatabase/CamelServiceDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.khtm.demo.camel.camelservicedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelServiceDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelServiceDatabaseApplication.class, args);
+    }
+
+}
